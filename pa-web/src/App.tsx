ed5ddb6b2dcef4f038477e831ec423bff4fcf327
tatianafischer <tatiana.fischer@acad.pucrs.br>
2021-07-11 10:38:33 -0300

    [feat] sass + bootstrap + landing page style finished

--- pa-web/src/App.tsx
+++ pa-web/src/App.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 
-import './styles/global.css';
-import './styles/pages/landing.css';
+import './styles/global.scss';
+import './styles/pages/landing.scss';
 
 import logo from './images/logo.svg';
 import mainImg from './images/landing.svg';
@@ -11,32 +11,27 @@ function App() {
 
     <div id="page-landing">
         <main id="page-landing-main">
-            <section className="content">
-                <button>
-                    <a href="" className="search-btn">
-                      <h2>BUSQUE PONTO -{'>'} </h2>
-                      
-                    </a>
-                </button>
-              <p>Encontre estabelecimentos que<br/> fazem parte da economia circular e <br/>são pontos de coleta de resíduos <br/>sólidos.</p>
+          
+            <section className="content" >
+                <button type="button" className="btn btn-primary btn-lg">Busque Ponto</button>
+                
+                <p>Encontre estabelecimentos que<br/> fazem parte da economia circular e <br/>são pontos de coleta de resíduos <br/>sólidos.</p>
             </section>
+
             <section className="content">
-              <button>
-                  <a href="" className="register-btn">
-                    <h2>CADASTRE PONTO -{'>'} </h2>
-                    
-                  </a>                
-              </button>
-              <p>Seja um ponto de coleta de <br/>residuos sólidos e faça sua parte <br/>na gestão de resíduos sólidos da <br/>cidade.</p>
+                <button type="button" className="btn btn-primary btn-lg">       Cadastre Ponto</button>
+                <p>Seja um ponto de coleta de <br/>residuos sólidos e faça sua parte <br/>na gestão de resíduos sólidos da <br/>cidade.</p>
             </section>
+
         </main>
+
         <div id="page-landing-logo">
           <section>
             <img src={logo} alt="Logo da marca Cyclo"/>
             <h1>Restaurantes <br/> amigos da <br/> terra</h1>
           </section>
           <section>
-            <img src={mainImg} alt="Planeta terra com uma lata de lixo"/>
+            <img id="landing-page-img" src={mainImg} alt="Planeta terra com uma lata de lixo" />
           </section>      
         </div>
     </div>
