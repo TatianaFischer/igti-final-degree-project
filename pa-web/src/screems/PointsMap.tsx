9496f1fcdc06e745c2a8f663940d55c232a26b2c
tatianafischer <tatiana.fischer@acad.pucrs.br>
2021-07-12 22:33:57 -0300

    [feat] PointsMap page

--- pa-web/src/screems/PointsMap.tsx
+++ pa-web/src/screems/PointsMap.tsx
@@ -1,10 +1,31 @@
 
-
+import logo from "../images/logo.svg"
+import { Link } from "react-router-dom";
+import { FiPlus } from "react-icons/fi";
 
 function PointsMap() {
 
   return (
-    <h1>mapa</h1>
+    <div id="page-map">
+
+      <aside>
+        <header>
+          <img src={logo} alt="Logo marcar Cyclo"/>
+
+          <p>Selecione no mapa  o restaurante e veja quais resíduos são coletados.</p>
+
+          <h3>Quais residuos podem ser coletados?</h3>
+          <li>Orgânicos</li>
+          <li>Sólidos (papel, latas, plásticos...)</li>
+          <li>Eletrônicos</li>
+        </header>
+      </aside>
+      <div></div>
+      <Link to="/cadastro" className="create-point">
+        <FiPlus size={32} color="#fff"/>
+      </Link>
+
+    </div>
   )
 }
 
