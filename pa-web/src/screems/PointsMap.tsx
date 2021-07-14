038395aa6a86bcc8ed6fb3079142947438830c71
tatianafischer <tatiana.fischer@acad.pucrs.br>
2021-07-14 09:31:21 -0300

    [feat] google map api

--- pa-web/src/screems/PointsMap.tsx
+++ pa-web/src/screems/PointsMap.tsx
@@ -1,7 +1,26 @@
-
+import 'leaflet/dist/leaflet.css'
 import logo from "../images/logo.svg"
 import { Link } from "react-router-dom";
 import { FiPlus } from "react-icons/fi";
+
+
+import { GoogleMap, LoadScript } from '@react-google-maps/api';
+
+import  "../styles/pages/pointsMap.scss";
+
+
+const containerStyle = {
+  width: '100vw',
+  height: '100vh'
+};
+
+const center = {
+  lat: -30.1087957,
+  lng: -51.3172272
+};
+
+
+const googleMapsAPIKey = process.env.REACT_APP_GOOGLE_MAPS_API_KEY;
 
 function PointsMap() {
 
@@ -11,18 +30,32 @@ function PointsMap() {
       <aside>
         <header>
           <img src={logo} alt="Logo marcar Cyclo"/>
-
           <p>Selecione no mapa  o restaurante e veja quais resíduos são coletados.</p>
-
-          <h3>Quais residuos podem ser coletados?</h3>
-          <li>Orgânicos</li>
-          <li>Sólidos (papel, latas, plásticos...)</li>
-          <li>Eletrônicos</li>
         </header>
+          
+          <ul><h3>Quais residuos podem ser coletados?</h3><li>Orgânicos</li>
+          <li>Sólidos (papel, latas, plásticos...)</li>
+          <li>Eletrônicos</li></ul>          
+        
       </aside>
-      <div></div>
+
+      <LoadScript
+      googleMapsApiKey={`${googleMapsAPIKey}`}         
+      >
+        <GoogleMap
+          mapContainerStyle={containerStyle}
+          center={center}
+          zoom={10}
+        >
+     
+          <></>
+        </GoogleMap>
+      </LoadScript>
+
+
+            
       <Link to="/cadastro" className="create-point">
-        <FiPlus size={32} color="#fff"/>
+        <FiPlus size={32} color="#fff" />
       </Link>
 
     </div>
